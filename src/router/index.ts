b21e8f19aa6a1d8f901e6175512c6072d7d7e538
Igor Veleski <igor.veleski@musala.com>
2025-04-23 17:31:51 +0200

    Initial setup

---
+++ src/router/index.ts
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import ProductList from '@/pages/ProductList.vue';
+import TheCart from '@/pages/TheCart.vue';
+import AddProduct from '@/pages/AddProduct.vue';
+
+const routes = [
+  { path: '/', name: 'Home', component: ProductList },
+  { path: '/cart', name: 'Cart', component: TheCart },
+  { path: '/add-product', name: 'AddProduct', component: AddProduct },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
